ae61ce610f9f8a51f6484e79817c440e3a0eb5ca
jan951002 <jatru95@gmail.com>
2018-05-24 12:31:40 -0500

    extension functions implemented

--- app/src/main/java/com/mvp/jan/util/ViewUtils.kt
+++ app/src/main/java/com/mvp/jan/util/ViewUtils.kt
@@ -1,4 +1,15 @@
 package com.mvp.jan.util
 
-class ViewUtils {
+import android.support.v7.app.AppCompatActivity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+
+fun ViewGroup.inflate(layoutId: Int): View {
+    return LayoutInflater.from(context).inflate(layoutId, this, false)
+}
+
+fun AppCompatActivity.toastShort(message: String) {
+    Toast.makeText(this, message ,Toast.LENGTH_SHORT).show()
 }
